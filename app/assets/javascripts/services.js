50b2a29ebd84ffb91cd08c0a4b22291affb4b55a
Thaisa Marinho <thaisa_marinho@hotmail.com>
2016-11-30 13:04:58 -0800

    add color to js code for status on services

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -3,11 +3,14 @@ jQuery(function() {
   $('.best_in_place').best_in_place()
   $('.best_in_place').bind('ajax:success', function(data) {
     var serviceId = $(this).data('id');
-
-    var budget = $("#best_in_place_service_17_budget").html();
-    var price = $("#best_in_place_service_17_price").html();
+    var budget = $(`#best_in_place_${serviceId}_budget`).html();
+    var price = $(`#best_in_place_${serviceId}_price`).html();
     status = budget - price
-    $('.status').html(status)
+    if (status <= 0) {
+      $(`#${serviceId} .status`).html(status).css({ 'color': 'red'})
+    } else {
+      $(`#${serviceId} .status`).html(status).css({ 'color': 'green'})
+    }
   })
 
 })
